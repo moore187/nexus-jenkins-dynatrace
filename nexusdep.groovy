84b42e32fb6a8693a45af4824f70580fad859a4d
Dominic Byrne <dominic@ecs-digital.co.uk>
2019-01-24 19:34:14 +0000

    Changed JSON output

--- nexusdep.groovy
+++ nexusdep.groovy
@@ -17,7 +17,7 @@ static def populatePomList() {
 
 static def populateVersionMap(ArrayList<File> pomList) {
 
-    def versionMap = [:]
+    def versionMapNew = [:]
     pomList.each {
     def currentPom = new File(it.toString())
     def xml = new XmlParser().parse(currentPom)
@@ -26,13 +26,13 @@ static def populateVersionMap(ArrayList<File> pomList) {
       dependency.children().each {
         def childString = it.name().toString()
         if (childString.indexOf("version") >= 0) {
-          versionMap << [(dependency.artifactId.text()):it.text()]
+          versionMapNew << [(dependency.artifactId.text()):it.text()]
         }
       }
     }
     }
-    println("VersionMap: ${versionMap.toString()}")
-    return versionMap
+    println("VersionMap: ${versionMapNew.toString()}")
+    return versionMapNew
 }
 
 static def findVersionsOnNexus (Map versionMap) {
@@ -89,93 +89,93 @@ static def findVersionsOnNexus (Map versionMap) {
 
     }
 
-    return nexusSet
+    return versionMap
     
 }
 
 //Paramiterise for NPM repos
-static def listRepositorys(){
+//static def listRepositorys(){
+//
+//    def nexusApiUrlRequest = new URL("http://localhost:8081/service/rest/v1/assets?repository=maven-releases").openConnection()
+//    def nexusApiRC = nexusApiUrlRequest.getResponseCode()
+//    def responseOutput = nexusApiUrlRequest.getInputStream().getText()
+//    if (nexusApiRC.equals(200)){
+//    println "http request successful. Repo Names will be populated"
+//    }
+//    else {
+//    println "http request failed. Please check the Nexus API"
+//    return 1
+//    }
+//    def json = new JsonSlurper().parseText(responseOutput)
+//    List<String> RepoNames = json.items
+//    nexusApiUrlRequest.disconnect()
+//    Map<String, Set> versionMap = new HashMap<String, Set>()
+//
+//    println("RepoNames: ${RepoNames.toString()}")
+//
+//    RepoNames.each {
+//
+//      String lib = ""
+//      String version = ""
+//      println("Current RepoNames Value: ${it.toString()}")
+//        if (it.path.contains(".jar") && !it.path.contains(".sha1")) {
+//
+//          String path = it["downloadUrl"]
+//            println(path.split("/"))
+//          version = path.split('/')[7]
+//          lib = path.split("/")[8].substring(0, path.split("/")[8].indexOf("-"))
+//
+//            if (versionMap.containsKey(lib)){
+//
+//                //Make sure version are sorted
+//
+//                Set set = new HashSet<String>()
+//                set.addAll(versionMap.get(lib))
+//                set.add(version)
+//                versionMap.replace(lib, set)
+//
+//            } else {
+//
+//                def set = new HashSet<String>()
+//                set.add(version)
+//                versionMap.put(lib, version)
+//
+//            }
+//
+//        }
+//
+//      println("VersionMap: ${versionMap.toString()}")
+//
+//    }
+//
+//    println("Edited VersionMap: ${versionMap.toString()}")
+//
+//  return versionMap
+//
+//}
+//
+//static def compareToNexus(Map<String,Object> buildVersionMap, Map<String, Set> repoNames) {
+//
+//    Set<String> nexusKeySet = new HashSet<String>(repoNames.keySet())
+//    Set<String> pomKeySet = new HashSet<String>(buildVersionMap.keySet())
+//
+//    println("Nexus Key Set: ${nexusKeySet.toString()}")
+//    println("POM Key Set: ${pomKeySet.toString()}")
+//
+//    nexusKeySet.retainAll(pomKeySet)
+//    Map<String, String> returnMap = new HashMap<>()
+//
+//    nexusKeySet.each {
+//
+//        if (repoNames.get(it)) returnMap[it] = (String) "Available Versions: ${repoNames.get(it).toString()}"
+//    }
+//
+//    println("returnMap: ${returnMap.toString()}")
+//    return returnMap
+//
+//}
 
-    def nexusApiUrlRequest = new URL("http://localhost:8081/service/rest/v1/assets?repository=maven-releases").openConnection()
-    def nexusApiRC = nexusApiUrlRequest.getResponseCode()
-    def responseOutput = nexusApiUrlRequest.getInputStream().getText()
-    if (nexusApiRC.equals(200)){
-    println "http request successful. Repo Names will be populated"
-    }
-    else {
-    println "http request failed. Please check the Nexus API"
-    return 1
-    }
-    def json = new JsonSlurper().parseText(responseOutput)
-    List<String> RepoNames = json.items
-    nexusApiUrlRequest.disconnect()
-    Map<String, Set> versionMap = new HashMap<String, Set>()
-
-    println("RepoNames: ${RepoNames.toString()}")
-
-    RepoNames.each {
-
-      String lib = ""
-      String version = ""
-      println("Current RepoNames Value: ${it.toString()}")
-        if (it.path.contains(".jar") && !it.path.contains(".sha1")) {
-
-          String path = it["downloadUrl"]
-            println(path.split("/"))
-          version = path.split('/')[7]
-          lib = path.split("/")[8].substring(0, path.split("/")[8].indexOf("-"))
-
-            if (versionMap.containsKey(lib)){
-
-                //Make sure version are sorted
-
-                Set set = new HashSet<String>()
-                set.addAll(versionMap.get(lib))
-                set.add(version)
-                versionMap.replace(lib, set)
-
-            } else {
-
-                def set = new HashSet<String>()
-                set.add(version)
-                versionMap.put(lib, version)
-
-            }
-
-        }
-
-      println("VersionMap: ${versionMap.toString()}")
-
-    }
-
-    println("Edited VersionMap: ${versionMap.toString()}")
-
-  return versionMap
-  
-}
-
-static def compareToNexus(Map<String,Object> buildVersionMap, Map<String, Set> repoNames) {
-
-    Set<String> nexusKeySet = new HashSet<String>(repoNames.keySet())
-    Set<String> pomKeySet = new HashSet<String>(buildVersionMap.keySet())
-
-    println("Nexus Key Set: ${nexusKeySet.toString()}")
-    println("POM Key Set: ${pomKeySet.toString()}")
-
-    nexusKeySet.retainAll(pomKeySet)
-    Map<String, String> returnMap = new HashMap<>()
-
-    nexusKeySet.each {
-
-        if (repoNames.get(it)) returnMap[it] = (String) "Available Versions: ${repoNames.get(it).toString()}"
-    }
-
-    println("returnMap: ${returnMap.toString()}")
-    return returnMap
-
-}
-
-static def urbancodeFileWriter(Map<String,Object> buildVersionMap, Map<String, String> repoNames) {
+static def urbancodeFileWriter(Map<String,String> buildVersionMap, Map<String, Set> repoNames) {
 
     def jsonText = new File('./file').getText()
     def slurper = new JsonSlurper().parseText(jsonText)
@@ -183,15 +183,11 @@ static def urbancodeFileWriter(Map<String,Object> buildVersionMap, Map<String, S
 
     // Change format to lib: Current=x, Latest=y
 
+    buildVersionMap.each {println(it)}
+
     buildVersionMap.each {
 
-        json.content.customProperties.putAt("${it.key}", "${it.value}")
-
-    }
-
-    repoNames.each {
-
-        json.content.customProperties.putAt("Nexus ${it.key}", "${it.value}")
+        json.content.customProperties.putAt("Current ${it.key} Version: ${it.value}", "Available ${it.key} Versions: ${repoNames[it.key]}")
 
     }
 
@@ -203,13 +199,12 @@ static def urbancodeFileWriter(Map<String,Object> buildVersionMap, Map<String, S
 static void main(String[] args) {
 
     PomList = populatePomList()
-    BuildVersionMap = populateVersionMap(PomList)
-//    RepoNames = listRepositorys()
-//    ComparedDependencies = compareToNexus(BuildVersionMap, RepoNames)
-    ComparedDependencies = findVersionsOnNexus(BuildVersionMap)
-    jsonText = urbancodeFileWriter(BuildVersionMap, ComparedDependencies)
+    Map<String,String> pomVersions = populateVersionMap(PomList)
+    Map<String, Set> ComparedDependencies = findVersionsOnNexus(pomVersions)
+    Map<String,String> pomVersionsNew = populateVersionMap(PomList)
+    jsonText = urbancodeFileWriter(pomVersionsNew, ComparedDependencies)
 
-    println(jsonText.toPrettyString())
+    //println(jsonText.toPrettyString())
 
     File file = new File("./file")
     file.bytes = []
